d6ac8615a488c0ab4252b740121256477b546424
heartkilla <artem.official.by@gmail.com>
2019-02-21 12:24:04 +0300

    Add video detection

--- detect.py
+++ detect.py
@@ -3,26 +3,28 @@
 Saves the detections in the `detection` folder.
 
 Usage:
-    python detect.py <iou threshold> <confidence threshold> <images>
+    python detect.py <images/video> <iou threshold> <confidence threshold> <filenames>
 
 Example:
-    python detect.py 0.5 0.5 data/images/dog.jpg data/images/office.jpg
+    python detect.py images 0.5 0.5 data/images/dog.jpg data/images/office.jpg
+    python detect.py video 0.5 0.5 data/video/shinjuku.mp4
+
+Note that only one video can be processed at one run.
 """
 
 import tensorflow as tf
 import sys
+import cv2
 
 from yolo_v3 import Yolo_v3
-from utils import load_images, load_class_names, draw_boxes
+from utils import load_images, load_class_names, draw_boxes, draw_frame
 
 _MODEL_SIZE = (416, 416)
 _CLASS_NAMES_FILE = './data/labels/coco.names'
 _MAX_OUTPUT_SIZE = 20
 
 
-def main(iou_threshold, confidence_threshold, img_names):
-    batch_size = len(img_names)
-    batch = load_images(img_names, model_size=_MODEL_SIZE)
+def main(type, iou_threshold, confidence_threshold, input_names):
     class_names = load_class_names(_CLASS_NAMES_FILE)
     n_classes = len(class_names)
 
@@ -31,21 +33,72 @@ def main(iou_threshold, confidence_threshold, img_names):
                     iou_threshold=iou_threshold,
                     confidence_threshold=confidence_threshold)
 
-    inputs = tf.placeholder(tf.float32,
-                            [batch_size, *_MODEL_SIZE, 3])
+    if type == 'images':
+        batch_size = len(input_names)
+        batch = load_images(input_names, model_size=_MODEL_SIZE)
+        inputs = tf.placeholder(tf.float32, [batch_size, *_MODEL_SIZE, 3])
+        detections = model(inputs, training=False)
+        saver = tf.train.Saver(tf.global_variables(scope='yolo_v3_model'))
 
-    detections = model(inputs, training=False)
+        with tf.Session() as sess:
+            saver.restore(sess, './weights/model.ckpt')
+            detection_result = sess.run(detections, feed_dict={inputs: batch})
 
-    saver = tf.train.Saver(tf.global_variables(scope='yolo_v3_model'))
+        draw_boxes(input_names, detection_result, class_names, _MODEL_SIZE)
 
-    with tf.Session() as sess:
-        saver.restore(sess, './weights/model.ckpt')
-        detection_result = sess.run(detections, feed_dict={inputs: batch})
+        print('Detections have been saved successfully.')
 
-    draw_boxes(img_names, detection_result, class_names, _MODEL_SIZE)
+    elif type == 'video':
+        inputs = tf.placeholder(tf.float32, [1, *_MODEL_SIZE, 3])
+        detections = model(inputs, training=False)
+        saver = tf.train.Saver(tf.global_variables(scope='yolo_v3_model'))
 
-    print('Detections have been saved successfully.')
+        with tf.Session() as sess:
+            saver.restore(sess, './weights/model.ckpt')
+
+            win_name = 'Video detection'
+            cv2.namedWindow(win_name)
+            cap = cv2.VideoCapture(input_names[0])
+            frame_size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+                          cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fourcc = cv2.VideoWriter_fourcc(*'X264')
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            out = cv2.VideoWriter('./detections/detections.mp4', fourcc, fps,
+                                  (int(frame_size[0]), int(frame_size[1])))
+
+            try:
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    resized_frame = cv2.resize(frame, dsize=_MODEL_SIZE,
+                                               interpolation=cv2.INTER_NEAREST)
+                    detection_result = sess.run(detections,
+                                                feed_dict={inputs: [resized_frame]})
+
+                    draw_frame(frame, frame_size, detection_result,
+                               class_names, _MODEL_SIZE)
+
+                    cv2.imshow(win_name, frame)
+
+                    key = cv2.waitKey(1) & 0xFF
+
+                    # Exit
+                    if key == ord('q'):
+                        break
+
+                    # Take screenshot
+                    if key == ord('s'):
+                        pass
+
+                    out.write(frame)
+            finally:
+                cv2.destroyAllWindows()
+                cap.release()
+
+    else:
+        raise ValueError("Inappropriate data type. Please choose either 'video' or 'images'.")
 
 
 if __name__ == '__main__':
-    main(float(sys.argv[1]), float(sys.argv[2]), sys.argv[3:])
+    main(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), sys.argv[4:])
--- utils.py
+++ utils.py
@@ -2,17 +2,17 @@
 
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+from seaborn import color_palette
+import cv2
 
 
 def load_images(img_names, model_size):
     """Loads images in a 4D array.
-
     Args:
         img_names: A list of images names.
         model_size: The input size of the model.
         data_format: A format for the array returned
             ('channels_first' or 'channels_last').
-
     Returns:
         A 4D NumPy array.
     """
@@ -39,16 +39,15 @@ def load_class_names(file_name):
 
 def draw_boxes(img_names, boxes_dicts, class_names, model_size):
     """Draws detected boxes.
-
     Args:
         img_names: A list of input images names.
         boxes_dict: A class-to-boxes dictionary.
         class_names: A class names list.
         model_size: The input size of the model.
-
     Returns:
         None.
     """
+    colors = ((np.array(color_palette("hls", 80)) * 255)).astype(np.uint8)
     for num, img_name, boxes_dict in zip(range(len(img_names)), img_names,
                                          boxes_dicts):
         img = Image.open(img_name)
@@ -60,7 +59,7 @@ def draw_boxes(img_names, boxes_dicts, class_names, model_size):
         for cls in range(len(class_names)):
             boxes = boxes_dict[cls]
             if np.size(boxes) != 0:
-                color = np.random.permutation([np.random.randint(256), 255, 0])
+                color = colors[cls]
                 for box in boxes:
                     xy, confidence = box[:4], box[4]
                     xy = [xy[i] * resize_factor[i % 2] for i in range(4)]
@@ -84,3 +83,36 @@ def draw_boxes(img_names, boxes_dicts, class_names, model_size):
         rgb_img = img.convert('RGB')
 
         rgb_img.save('./detections/detection_' + str(num + 1) + '.jpg')
+
+
+def draw_frame(frame, frame_size, boxes_dicts, class_names, model_size):
+    """Draws detected boxes in a video frame.
+    Args:
+        frame: A video frame.
+        frame_size: A tuple of (frame width, frame height).
+        boxes_dicts: A class-to-boxes dictionary.
+        class_names: A class names list.
+        model_size:The input size of the model.
+    Returns:
+        None.
+    """
+    boxes_dict = boxes_dicts[0]
+    resize_factor = (frame_size[0] / model_size[1], frame_size[1] / model_size[0])
+    colors = ((np.array(color_palette("hls", 80)) * 255)).astype(np.uint8)
+    for cls in range(len(class_names)):
+        boxes = boxes_dict[cls]
+        color = colors[cls]
+        color = tuple([int(x) for x in color])
+        if np.size(boxes) != 0:
+            for box in boxes:
+                xy = box[:4]
+                xy = [int(xy[i] * resize_factor[i % 2]) for i in range(4)]
+                cv2.rectangle(frame, (xy[0], xy[1]), (xy[2], xy[3]), color[::-1], 2)
+                (test_width, text_height), baseline = cv2.getTextSize(class_names[cls],
+                                                                      cv2.FONT_HERSHEY_SIMPLEX,
+                                                                      0.75, 1)
+                cv2.rectangle(frame, (xy[0], xy[1]),
+                              (xy[0] + test_width, xy[1] - text_height - baseline),
+                              color[::-1], thickness=cv2.FILLED)
+                cv2.putText(frame, class_names[cls], (xy[0], xy[1] - baseline),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
